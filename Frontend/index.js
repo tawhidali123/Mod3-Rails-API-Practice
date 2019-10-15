c65c39e1601369f6e1f95e6c19bcb076e716d864
Tawhid <gem@tawhid.local>
2019-10-15 14:56:51 -0400

    done

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,5 +1,6 @@
 // Code your solution here
 let shoeUl = document.querySelector("#shoe-list");
+let mainShoe = document.querySelector("#main-shoe");
 
 
 fetch("http://localhost:3000/shoes")
@@ -7,71 +8,102 @@ fetch("http://localhost:3000/shoes")
 .then(response => {
     let res = response;
     console.log(res)
-    // let shoeUl = document.querySelector("#shoe-list");
-    // let mainShoe = document.querySelector("#main-shoe");
 
     for (const i of res) { 
         shoeUl.innerHTML += `
         <li class="list-group-item" data-num="${i.id}">${i.name}</li>
         `;
        
-    }
-
-    // shoeUl.addEventListener("click", (event) => {
-    //     console.log(event.target.dataset.num)
-    //     console.log(mainShoe)
-    //     let number = event.target.dataset.num
-    
-    //     fetch(`"https://localhost:3000/shoes/${number}"`, {})
-    //     .then(res => res.json())
-    //     .then(response => {
-    //         console.log("in here")
-    //     })
-    
-        
-    
-    // })
+    }  
     
 })
 
-// let shoeUl = document.querySelector("#shoe-list");
-// let mainShoe = document.querySelector("#main-shoe");
 
+// for every shoe I made add an event 
+shoeUl.addEventListener("click", (event) => {
+    console.log(event.target.dataset.num)
+    console.log(mainShoe)
+    let number = event.target.dataset.num
 
-// shoeUl.addEventListener("click", (event) => {
-//     console.log(event.target.dataset.num)
-//     console.log(mainShoe)
-//     let number = event.target.dataset.num
+    // retrieve each shoe info
+    fetch(`http://localhost:3000/shoes/${number}`)
+    .then(res => res.json())
+    .then(response => {
+        console.log(response);
 
-//     fetch(`"https://localhost:3000/shoes/${number}"`, {})
-//     .then(res => res.json())
-//     .then(response => {
-//         console.log("in here")
-//     })
+        // Main section where shoe info will go
+        mainShoe.innerHTML = `
+        <img class="card-img-top" id="shoe-image" src="${response.image}" alt="Shoe Image Goes Here">
+        <div class="card-body">
+        <h4 class="card-title" id="shoe-name">${response.name}</h4>
+        <p class="card-text" id="shoe-description">${response.description}</p>
+        <p class="card-text"><small class="text-muted" id="shoe-price">${response.price}</small></p>
+        <div class="container" id="form-container">
 
+        <form id="new-review">
+        <div class="form-group">
+        <textarea class="form-control" id="review-content" rows="3"></textarea>
+        <input type="submit" class="btn btn-primary"></input>
+        </div>
+        </form>
 
-// })
+        </div>
 
-setTimeout(() => {
+        </div>
+        <h5 class="card-header">Reviews</h5>
+        <ul class="list-group list-group-flush" id="reviews-list">
+            
+        </ul>
+        `;
 
-    // let shoeUl = document.querySelector("#shoe-list");
-    let mainShoe = document.querySelector("#main-shoe");
-
-
-    shoeUl.addEventListener("click", (event) => {
-        console.log(event.target.dataset.num)
-        console.log(mainShoe)
-        let number = event.target.dataset.num
-
-        fetch(`"https://localhost:3000/shoes/1"`, {})
-        .then(res => res.json())
-        .then(response => {
-            console.log("in here")
+        let reviewSelect = document.querySelector("#reviews-list");
+        
+        response.reviews.forEach(review => {
+            reviewSelect.innerHTML += `
+            <li class="list-group-item">
+            ${review.content}
+            </li>
+            `;
         })
+
+        let formSelector = document.querySelector("#new-review");
+
+        // submit form
+        formSelector.addEventListener("submit", (evt) => {
+            evt.preventDefault();
+            console.log(evt.target);
+            
+            let text = evt.target["review-content"].value;
+
+            if(evt.target.tagName === "FORM") {
+                fetch("http://localhost:3000/shoes/1/reviews", {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({content: text})
+                })
+                .then(res => res.json())
+                .then(response => {
+                    console.log("success");
+                    reviewSelect.innerHTML += `
+                    <li class="list-group-item">${response.content}
+                        <button class="delete">Delete</button>
+                        <button class="edit">Edit</button>
+                    </li>
+                    `;
+                })
+            }
+ 
+        })
+
+        // let destroy = document.getElementsByClassName("delete");
+        // destroy.addEventListener("click", () => {console.log("we going to destroy this in a bit")})
+
 
     })
 
-}, 1500)
+})
 
 
 
@@ -87,28 +119,41 @@ setTimeout(() => {
 
 
 
- // make display hidden
-//  mainShoe.innerHTML += `
-//  <div class="card mb-3" id="main-shoe" style=display: hidden>
 
-//    <img class="card-img-top" id="shoe-image" src="${i.image}" alt="Shoe Image Goes Here">
-//    <div class="card-body">
-//      <h4 class="card-title" id="shoe-name">${i.name}</h4>
-//      <p class="card-text" id="shoe-description">${i.description}</p>
-//      <p class="card-text"><small class="text-muted" id="shoe-price">${i.price}</small></p>
-//      <div class="container" id="form-container">
-//        <!-- FORM GOES HERE -->
-//      </div>
+// setTimeout(() => {
+
+//     // let shoeUl = document.querySelector("#shoe-list");
+//     let mainShoe = document.querySelector("#main-shoe");
 
 
-//    </div>
-//    <h5 class="card-header">Reviews</h5>
-//    <ul class="list-group list-group-flush" id="reviews-list">
-//      <!-- REVIEWS GO HERE -->
-//    </ul>
-//  </div>
+//     shoeUl.addEventListener("click", (event) => {
+//         console.log(event.target.dataset.num)
+//         console.log(mainShoe)
+//         let number = event.target.dataset.num
 
-// `
+//         fetch(`"https://localhost:3000/shoes/${}"`, {})
+//         .then(res => res.json())
+//         .then(response => {
+//             console.log("in here")
+//         })
+
+//     })
+
+// }, 1500)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
